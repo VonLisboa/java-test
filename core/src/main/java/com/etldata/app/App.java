fa60b2793ecacfe2d2329d5b449d7650f8fea98a
Glaucio Almeida <glaucio@mail.com>
2023-01-31 19:41:04 -0300

    App

--- core/src/main/java/com/etldata/app/App.java
+++ core/src/main/java/com/etldata/app/App.java
@@ -1,9 +1,11 @@
 package com.etldata.app;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
-import com.etldata.integrations.destinations.DynamicMysql;
+import com.etldata.integrations.destinations.DynamicCsv;
 import com.etldata.integrations.destinations.LocalStorage;
 import com.etldata.integrations.sources.openmeteo.MeteoApi;
 import com.etldata.integrations.sources.rest.GenericRestClient;
@@ -14,18 +16,18 @@ public class App {
     Dotenv dotenv = Dotenv.load();
     static MeteoApi meteo = new MeteoApi();
     static GenericRestClient rest = new GenericRestClient("https://", "api.open-meteo.com", "v1");
-    static DynamicMysql mysql = new DynamicMysql();
+    static DynamicCsv csv = new DynamicCsv();
 
     public static void main(String[] args) throws IOException {
         try {
-            LocalStorage store = new LocalStorage("test.json");
+            LocalDateTime now = LocalDateTime.now();  
+            LocalStorage store = new LocalStorage("meteo"+now+".json");
 
-            Map json = rest.get("forecast", "latitude=52.52&longitude=13.41&current_weather=true");
-
-            //Map json = meteo.forecast("latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
-            store.saveJson(json);
+            Map json1 = rest.get("forecast", "latitude=52.52&longitude=13.41&current_weather=true");
+            Map json2 = meteo.forecast("latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
             
-            mysql.saveToTable(json);
+            store.saveJson(json1);
+            csv.saveToTable(json2);
         } catch (Exception e) {
             e.printStackTrace();
         }
